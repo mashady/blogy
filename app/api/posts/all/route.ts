180d1cd6be18d02bd1eaeff055a84dc4f80c0e61
mashady <muhammedmashady@gmail.com>
2024-07-01 11:10:04 +0300

    0

--- app/api/posts/all/route.ts
+++ app/api/posts/all/route.ts
@@ -1,11 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-
+import NextCors from "nextjs-cors";
 const prisma = new PrismaClient();
 
-export async function GET(req: any, res: any) {
+export async function GET(req, res) {
   const skip = parseInt(req.nextUrl.searchParams.get("skip")) || 0;
   const take = parseInt(req.nextUrl.searchParams.get("take")) || 5;
+  const response = NextResponse.json({ message: "Hello, world!" });
+  // Set CORS headers
+  response.headers.set("Access-Control-Allow-Credentials", "true");
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, baggage"
+  );
 
   const posts = await prisma.post.findMany({
     orderBy: { createdAt: "desc" },
@@ -24,4 +36,19 @@ export async function GET(req: any, res: any) {
     },
   }));
   return NextResponse.json(transformedPosts);
+}
+
+export async function OPTIONS() {
+  const response = new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+      "Access-Control-Allow-Headers":
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, baggage",
+    },
+  });
+
+  return response;
 }
--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,50 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  /**
+   * 
   const currentUser = request.cookies.get("authjs.session-token")?.value;
   console.log("currentUser");
   console.log(currentUser);
   if (!currentUser) {
     return NextResponse.redirect(new URL("/forbidden", request.url));
   }
+
+   */
+
+  // handle cors
+  const res = NextResponse.next();
+
+  // add the CORS headers to the response
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT"
+  );
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  return res;
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/settings", "/settings/profile", "/new-post", "/profile"],
+  matcher: "/api/:path*",
 };
+
+/**
+export const config = {
+  matcher: [
+    "/settings",
+    "/settings/profile",
+    "/new-post",
+    "/profile",
+    "/api/:path*",
+  ],
+};
+
+
+*/
