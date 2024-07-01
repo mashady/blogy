060acf771486cdf8df40ad8a167c8c51876b6026
mashady <muhammedmashady@gmail.com>
2024-07-01 06:13:34 +0300

    add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        poppins: "var(--font-poppins)",
+      },
+
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
